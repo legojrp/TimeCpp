85f045685755d603fb48356ec8ff92b2084ac7fe
legojrp <84683257+legojrp@users.noreply.github.com>
2021-08-08 21:43:52 -0400

    Update app.cpp

--- app.cpp
+++ app.cpp
@@ -1,1 +1,15 @@
 //For testing and debugging only
+#include "time.cpp"
+#include <ctime>
+#include <iostream>
+#include <string>
+using namespace std;
+int main () {
+  time_t rawtime;
+	time(&rawtime);
+std::string overall = ctime(&rawtime);
+std::string weekday, month, day, time, year;
+  cout << ctime(&rawtime);
+  Time current;
+  cout << current.testing();
+};
